1bbdbed805d1b9fa06b2142315cf8c9bc9dd5eb1
Trond Aasan <trond.aasan@gmail.com>
2020-12-04 02:54:07 +0100

    Bruk ANSI fargekoder

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,12 @@ module Main where
 
 import Prelude
 import Advent (Door(..), open)
+import Ansi.Codes (Color(..))
+import Ansi.Output (bold, foreground, underline, withGraphics)
 import Data.Maybe (Maybe(..))
 import Data.Traversable (traverse_)
 import Data.TraversableWithIndex (traverseWithIndex)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console (log)
 import Node.Encoding (Encoding(..))
@@ -18,13 +21,29 @@ main = do
 openDoors :: Effect (Array String)
 openDoors = traverseWithIndex (\i -> openDoor (i + 1)) doors
 
-doors :: Array (String -> Door)
-doors = [ Door1, Door2, Door3 ]
+doors :: Array (Tuple (String -> Door) (Maybe String))
+doors =
+  [ Tuple Door1 (Just "[1020036,286977330]")
+  , Tuple Door2 (Just "[645,737]")
+  , Tuple Door3 (Just "[169.0,7560370818.0]")
+  ]
 
-openDoor :: Int -> (String -> Door) -> Effect String
-openDoor day door = do
+openDoor :: Int -> Tuple (String -> Door) (Maybe String) -> Effect String
+openDoor day (Tuple door correct) = do
   input <- getInput day
-  pure $ show day <> "\t" <> output (open (door input))
+  let
+    answer = open (door input)
+  let
+    color =
+      if correct == Nothing then
+        BrightYellow
+      else if answer == correct then
+        BrightGreen
+      else
+        BrightRed
+  let
+    ansi = withGraphics (bold <> underline <> foreground color)
+  pure $ ansi $ show day <> "\t" <> output answer
   where
   output (Just x) = x
 
